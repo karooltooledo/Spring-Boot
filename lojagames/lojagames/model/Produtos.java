e26b34db58e0daadb44575457ad3bd6c96d6fcc5
Karoline Toledo <82004773+karooltooledo@users.noreply.github.com>
2021-08-09 15:34:29 -0300

    Add files via upload

---
+++ lojagames/lojagames/model/Produtos.java
@@ -0,0 +1,87 @@
+package br.org.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+	@Entity
+	@Table(name = "tb_produtos")
+	public class Produtos {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		
+		@NotNull
+		private String descricao;
+		
+		private double preco;
+		
+		private String nome;
+
+		@ManyToOne
+		@JsonIgnoreProperties("produtos")
+		private Categoria categoria;
+		
+		@ManyToOne
+		@JsonIgnoreProperties("produtos")
+		private Usuario usuario;
+		
+				
+		public Usuario getUsuario() {
+			return usuario;
+		}
+
+		public void setUsuario(Usuario usuario) {
+			this.usuario = usuario;
+		}
+
+		public Categoria getCategoria() {
+			return categoria;
+		}
+
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+
+		public double getPreco() {
+			return preco;
+		}
+
+		public void setPreco(double preco) {
+			this.preco = preco;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+			
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+		
+}
